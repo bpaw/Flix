1031d67d3dc9c485a2187efed2ef40394b9fcb3b
bpaw <bpaw.dev@gmail.com>
2017-11-18 01:26:10 -0800

    finished required user stories

---
+++ flix/MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  flix
+//
+//  Created by Brandon Paw on 11/18/17.
+//  Copyright © 2017 Brandon Paw. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
