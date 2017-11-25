1f40b0aa63194f68aba489b845ae69fc3fcd4830
bpaw <bpaw.dev@gmail.com>
2017-11-24 22:27:55 -0800

    collection view setup

---
+++ flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Brandon Paw on 11/24/17.
+//  Copyright © 2017 Brandon Paw. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
